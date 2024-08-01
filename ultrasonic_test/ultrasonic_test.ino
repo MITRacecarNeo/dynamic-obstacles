f3663365877d6c4159e77e3c60082765e3640ddb
Kevin Yu <kevinyu567@gmail.com>
2024-08-01 16:02:10 -0400

    Format and update ultrasonic test to test both sensors

--- ultrasonic_test/ultrasonic_test.ino
+++ ultrasonic_test/ultrasonic_test.ino
@@ -1,27 +1,35 @@
-const int trigPin = 2;  
-const int echoPin = 3; 
-float duration, distance; 
+const int trigPinL = 2;
+const int echoPinL = 3;
 
-void setup() {  
+const int trigPinR = 4;
+const int echoPinR = 5;
 
-	pinMode(trigPin, OUTPUT);  
-	pinMode(echoPin, INPUT);  
-	Serial.begin(9600);  
-}  
+void setup() {
+  pinMode(trigPinL, OUTPUT);
+  pinMode(echoPinL, INPUT);
+  pinMode(trigPinR, OUTPUT);
+  pinMode(echoPinR, INPUT);
+  Serial.begin(9600);
+}
 
-
-void loop() {  
-
-	digitalWrite(trigPin, LOW);
+float getDistance(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration*.0343)/2;
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  delay(100);
+  float duration = pulseIn(echoPin, HIGH);
+  return (duration * 0.0343) / 2;
+}
 
+void loop() {
+  float lDist = getDistance(trigPinL, echoPinL);
+  float rDist = getDistance(trigPinR, echoPinR);
+
+  Serial.print("L: ");
+  Serial.print(lDist, 3);
+  Serial.print(" R: ");
+  Serial.println(rDist, 3);
+  delay(100);
 }
